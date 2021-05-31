98790d5ec1a370a28bab6bcf1afca61a38bd23e0
tenjodiego961 <79155327+tenjodiego961@users.noreply.github.com>
2021-05-30 22:02:56 -0500

    add page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+import Logo from '../assets/images/logo.svg';
+
+function Navbar() {
+    return (
+        <ContainerNav>
+            <LogoImg>
+                <img src={Logo} alt="Candy Fruit"></img>
+            </LogoImg>
+            <Menu>
+                <FontAwesomeIcon icon={faBars} />
+                <span>Menu</span>
+            </Menu>
+        </ContainerNav>
+    )
+}
+
+export default Navbar;
+
+// Styles
+
+const ContainerNav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    position: fixed;
+    right: 0;
+    left: 0;
+`
+
+const LogoImg = styled.figure`
+    width: 180px;
+    height: 150px;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: container;
+    }
+`
+const Menu = styled.button`
+    cursor: pointer;
+    background: none;
+    padding: 8px;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    font-size: 15px;
+    transition: 250ms;
+
+    span {
+        margin-left: 5px;
+    }
+    &:hover {
+        transform: scale(1.3);
+        opacity: 0.95;
+    }
+`
---
+++ src/components/Section.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components';
+
+import Background from '../assets/images/background-home.jpg'
+
+function Section() {
+    return (
+        <Wrap>
+            <ItemText>
+                <h1>Disfruta de tu Snack con Tranquilidad</h1>
+                <h3>Prueba Candy Fruit</h3>
+            </ItemText>
+            <DownArrow>
+            🡫
+            </DownArrow>
+        </Wrap>
+    )
+}
+
+export default Section;
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-position: center;
+    background-image: url(${Background});
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const ItemText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 30vh;
+    text-shadow: 0 0 15px #fefefe;
+`
+const DownArrow = styled.div`
+    font-size: 60px;
+    color: #000;
+    animation: animateDown infinite 1.5s;
+`
